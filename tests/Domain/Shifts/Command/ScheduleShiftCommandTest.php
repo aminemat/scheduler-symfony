23eb7e0756284e14a9d7dcbdae35dc7801d366fd
Amine Matmati <amine.matmati@nerdery.com>
2016-04-01 00:38:49 -0500

    Added unit test for ScheduleShiftCommand

---
+++ tests/Domain/Shifts/Command/ScheduleShiftCommandTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test\Domain\Shifts\Command;
+
+use DateTime;
+use Domain\Shifts\Commands\ScheduleShiftCommand;
+use Domain\Shifts\Entities\DateRange;
+use \InvalidArgumentException;
+use \PHPUnit_Framework_TestCase;
+
+class ScheduleShiftCommandTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function test_throws_an_exception_when_startdate_precedes_enddate()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new ScheduleShiftCommand(
+            'foo',
+            new DateRange(
+                new DateTime('January 1st 2016 3:00 p.m'),
+                new DateTime('January 1st 2016 1:00 p.m')
+            )
+        );
+    }
+}
